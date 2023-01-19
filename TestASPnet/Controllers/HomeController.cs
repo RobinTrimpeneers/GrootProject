2220993524501ebbc331d300931cbfca88564880
RobinTrimpeneers <robin.trimpeneers@leerling.hasp-o.be>
2023-01-19 11:27:17 +0100

    Add project files.

---
+++ TestASPnet/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestASPnet.Models;
+
+namespace TestASPnet.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
